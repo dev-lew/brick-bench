4c90b1288e0de26cdd20c74369188f02e04a664d
devlew <dev.lewis611@gmail.com>
2025-03-26 22:07:11 -0400

    Add both cdi and regular nvidia runtimeclass support
    Begin pv

---
+++ chameleon-deployment/run-playbooks.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+usage=$(cat <<EOF
+run-playbooks.sh [-t HUGGINGFACE_TOKEN] (optional)
+
+Provide a Hugging Face token if you want the brick-bench
+playbook to download models directly onto the host. If not
+set, you will have to copy them yourself.
+EOF
+     )
+
+while getopts t: optarg; do
+    case "${optarg}" in
+        t) token="${OPTARG}"
+           ;;
+        *) echo "${usage}"; exit 1
+    esac
+done
+
+VENV_PATH="${HOME}/.virtualenvs/brick-bench"
+
+if ! . "${VENV_PATH}/bin/activate"; then
+    echo "Failed to source virtualenv. Is VENV_PATH set correctly?"
+    exit 1
+fi
+
+printf "Setting ansible enviroment variables... "
+export ANSIBLE_CONFIG="ansible/ansible.cfg"
+export ANSIBLE_HOME="${VENV_PATH}"
+echo "done"
+
+ansible-galaxy install -r deps/requirements.yaml
+
+echo "Running ansible-playbooks"
+ansible-playbook ansible/kubernetes.yaml
+ansible-playbook ansible/brick-bench.yaml "${token:+--extra-vars ${token}}"
