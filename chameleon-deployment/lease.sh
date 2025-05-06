291def80bd346b7f7db377302aed28ce004f22af
dev-lew <>
2025-05-06 11:56:53 -0400

    Update scripts to handle both cpu and gpu reservations

--- chameleon-deployment/lease.sh
+++ chameleon-deployment/lease.sh
@@ -1,18 +1,41 @@
 #!/bin/sh
 
+usage=$(cat <<EOF
+lease.sh <lease-name> [-g] (optional)
+
+-g
+    Reserve a node with gpu. Defaults to 2 cpu nodes.
+EOF
+     )
+
 lease_name=$1
 
 if [ -z "${lease_name}" ]; then
     echo "You must provide a lease name"
+    echo "${usage}"
     exit 1
 fi
 
-END_DATE="$(date --date '+7 days' '+%F %H:%M')"
+while getopts g: optarg; do
+    case "${optarg}" in
+        g) use_gpu=true
+           ;;
+        *) echo "${usage}"; exit 1
+    esac
+done
 
-openstack reservation lease create \
-          --reservation min=1,max=1,resource_type=physical:host,resource_properties='["=", "$node_type", "compute_cascadelake_r"]' \
-	  --end-date "${END_DATE}" "${lease_name}-cpu"
+END_DATE="$(date --date '+1 days' '+%F %H:%M')"
 
-openstack reservation lease create \
-          --reservation min=1,max=1,resource_type=physical:host,resource_properties='["=", "$node_type", "gpu_rtx_6000"]' \
-	  --end-date "${END_DATE}" "${lease_name}-gpu"
+if [ "${use_gpu}" = true ]; then
+    openstack reservation lease create \
+              --reservation min=1,max=1,resource_type=physical:host,resource_properties='["=", "$node_type", "compute_cascadelake_r"]' \
+	      --end-date "${END_DATE}" "${lease_name}-cpu"
+
+    openstack reservation lease create \
+              --reservation min=1,max=1,resource_type=physical:host,resource_properties='["=", "$node_type", "gpu_rtx_6000"]' \
+	      --end-date "${END_DATE}" "${lease_name}-gpu"
+else
+    openstack reservation lease create \
+              --reservation min=2,max=2,resource_type=physical:host,resource_properties='["=", "$node_type", "compute_cascadelake_r"]' \
+	      --end-date "${END_DATE}" "${lease_name}-cpu"
+fi
