af40463a1c620ddf5d183ce3dd30af94c025e859
dev-lew <>
2025-05-29 15:22:37 -0400

    Fully support cpu inference
    Tested and working

--- chameleon-deployment/run-playbooks.sh
+++ chameleon-deployment/run-playbooks.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
 
 usage=$(cat <<EOF
-run-playbooks.sh [-t HUGGINGFACE_TOKEN] (optional)
+run-playbooks.sh [-g] [-t HUGGINGFACE_TOKEN] (optional)
 
-Provide a Hugging Face token if you want the brick-bench
+-g: Enable support for GPU inference (disabled by default).
+
+-t: Provide a Hugging Face token if you want the brick-bench
 playbook to download models directly onto the host. If not
 set, you will have to copy them yourself.
 EOF
      )
 
-while getopts t: optarg; do
+while getopts gt: optarg; do
     case "${optarg}" in
+        g) use_gpu=true
+           ;;
         t) token="${OPTARG}"
            ;;
         *) echo "${usage}"; exit 1
@@ -33,10 +37,20 @@ ansible-galaxy install -r deps/requirements.yaml
 
 echo "Running ansible-playbooks"
 
-ansible-playbook ansible/kubernetes.yaml
+#ansible-playbook ansible/kubernetes.yaml
+
+extra_vars=""
+skip_tags=""
+
+if [ "${use_gpu}" = true ]; then
+    extra_vars="${extra_vars} image_gen_manifest=image-gen-gpu.yaml"
+else
+    extra_vars="${extra_vars} image_gen_manifest=image-gen-cpu.yaml"
+    skip_tags="${skip_tags}gpu,"
+fi
 
 if [ -n "${token}" ]; then
-    ansible-playbook ansible/brick-bench.yaml --extra-vars "token=${token}"
-else
-    ansible-playbook ansible/brick-bench.yaml
+    extra_vars="${extra_vars} token=${token}"
 fi
+
+ansible-playbook ansible/brick-bench.yaml --extra-vars "${extra_vars}" --skip-tags="${skip_tags}"
