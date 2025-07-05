d39bbf5aa5119ac4c7cddab527607ab1a2f73460
dev-lew <>
2025-07-05 18:06:29 -0400

    Create locustfile

---
+++ chameleon-deployment/src/client/locustfile.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+from comfy_api import ComfyUIRequest
+from locust import HttpUser, task
+
+
+class GenerateImageRequest(HttpUser):
+    hostname = "stable-diffusion-comfy-ui.brick-bench.svc.cluster.local"
+
+    @task
+    def generate_image(self):
+        self.client.post(self.hostname, data=ComfyUIRequest())
