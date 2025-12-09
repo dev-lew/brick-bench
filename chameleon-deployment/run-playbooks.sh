a9a0ab3aa4f7cc78ab41a1992ea5f9c92802229e
devlew <dev.lewis611@gmail.com>
2025-12-09 15:08:51 -0500

    Let uv handle the virtualenv

--- chameleon-deployment/run-playbooks.sh
+++ chameleon-deployment/run-playbooks.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-usage=$(cat <<EOF
+usage=$(
+    cat <<EOF
 run-playbooks.sh [-g] [-t HUGGINGFACE_TOKEN] (optional)
 
 -g: Enable support for GPU inference (disabled by default).
@@ -9,35 +10,32 @@ run-playbooks.sh [-g] [-t HUGGINGFACE_TOKEN] (optional)
 playbook to download models directly onto the host. If not
 set, you will have to copy them yourself.
 EOF
-     )
+)
 
 while getopts gt: optarg; do
     case "${optarg}" in
-        g) use_gpu=true
-           ;;
-        t) token="${OPTARG}"
-           ;;
-        *) echo "${usage}"; exit 1
+    g)
+        use_gpu=true
+        ;;
+    t)
+        token="${OPTARG}"
+        ;;
+    *)
+        echo "${usage}"
+        exit 1
+        ;;
     esac
 done
 
-VENV_PATH=".venv"
-
-if ! . "${VENV_PATH}/bin/activate"; then
-    echo "Failed to source virtualenv. Is VENV_PATH set correctly?"
-    exit 1
-fi
-
 printf "Setting ansible enviroment variables... "
 export ANSIBLE_CONFIG="ansible/ansible.cfg"
-export ANSIBLE_HOME="${VENV_PATH}"
 echo "done"
 
-ansible-galaxy install -r ansible/deps/requirements.yaml
+uv run ansible-galaxy install -r ansible/deps/requirements.yaml
 
-echo "Running ansible-playbooks"
+echo "Running ansible playbooks"
 
-ansible-playbook ansible/kubernetes.yaml
+uv run ansible-playbook ansible/kubernetes.yaml
 
 extra_vars=""
 skip_tags=""
@@ -53,4 +51,4 @@ if [ -n "${token}" ]; then
     extra_vars="${extra_vars} token=${token}"
 fi
 
-ansible-playbook ansible/brick-bench.yaml --extra-vars "${extra_vars}" --skip-tags="${skip_tags}"
+uv run ansible-playbook ansible/brick-bench.yaml --extra-vars "${extra_vars}" --skip-tags="${skip_tags}"
